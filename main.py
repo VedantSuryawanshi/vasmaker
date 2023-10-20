4caa129bae654452eaaa017800a9eadb9e0b8b7b
vedantsuryawanshi <hellovedantsuryawanshi@gmail.com>
2023-10-20 09:21:28 +0530

    commit

--- main.py
+++ main.py
@@ -4,118 +4,116 @@ from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 import json
 
-# Load CSV data into a DataFrame
-csv_file = 'data.csv'
-# Specify the columns you want to use
-selected_columns = ["Full name", "Next service", "Insurance", "PUC", "Workshop", "Avg run/month", "Fuel type"]
-df = pd.read_csv(csv_file, usecols=selected_columns)
-
-# Load the custom JSON template
-json_template_file = 'manifest.json'
-with open(json_template_file, 'r') as json_template:
-    json_template_data = json.load(json_template)
-
-# Load the Jinja2 template
-env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('demo.html')
-
-# Convert "Fuel Avg run/month" to numeric (float) with a default of 500 if missing or non-numeric
-df["Avg run/month"] = pd.to_numeric(df["Avg run/month"], errors="coerce").fillna(500).astype(int)
-
-# Create a directory to store individual HTML files (if it doesn't exist)
-output_dir = 'output_html'
-os.makedirs(output_dir, exist_ok=True)
-
-# Fuel cost based on fuel type (PUC renewal cost)
-fuel_cost_per_type = {
+# Modify variable names in the code
+new_csv_file = 'vas.csv'
+new_selected_columns = ["FULL NAME", "NEXT SERVICE", "INSURANCE", "PUC", "WORKSHOP", "FUEL","MAK"]
+new_json_template_file = 'manifest.json'
+new_env = Environment(loader=FileSystemLoader('.'))
+new_template = new_env.get_template('demo.html')
+new_output_dir = 'output_html'
+new_fuel_cost_per_type = {
     "Petrol": 125,
     "Diesel": 150,
     "CNG": 125,
-    "Electric": 0  # Assuming electric vehicles have no PUC renewal cost
+    "Electric": 0
 }
+new_mileage = 12
+new_fuel_cost_per_liter = 105
 
-# Fuel mileage (km per liter)
-mileage = 12
+# Load CSV data into a DataFrame
+df = pd.read_csv(new_csv_file, usecols=new_selected_columns)
+print(df.columns)
 
-# Fuel cost per liter (assuming 105 INR per liter)
-fuel_cost_per_liter = 105
+# Load the custom JSON template
+with open(new_json_template_file, 'r') as json_template:
+    json_template_data = json.load(json_template)
+
+# Convert "Fuel Avg run/month" to numeric (float) with a default of 500 if missing or non-numeric
+df["FUEL"] = pd.to_numeric(df["FUEL"], errors="coerce").fillna(500).astype(int)
+
+# Create a directory to store individual HTML files (if it doesn't exist)
+os.makedirs(new_output_dir, exist_ok=True)
 
 # Loop through each row in the DataFrame
 for index, row in df.iterrows():
     # Prepare data for the template (one row at a time)
     data = row.to_dict()
 
-    # Set a default value for "Preferred Garage" and update the link
-    if pd.isna(data['Workshop']) or data['Workshop'] == '':
-        data['Workshop'] = 'Workshop'
+# Set a default value for "Preferred Garage" and update the link
+    if pd.isna(data['WORKSHOP']) or data['WORKSHOP'] == '':
+        data['WORKSHOP'] = 'WORKSHOP'
         data['Link'] = 'https://cars.mekit.in/bookatgarageform'
-    elif data['Workshop'] == 'Express Autocare':
+    elif data['WORKSHOP'] == 'Express Autocare':
         data['Link'] = 'https://cars.mekit.in/bookatexpressautocare'
-    elif data['Workshop'] == 'Yadav Garage':
+    elif data['WORKSHOP'] == 'Yadav Garage':
         data['Link'] = 'https://cars.mekit.in/bookatyadavgarage'
-    elif data['Workshop'] == 'Wash and More':
+    elif data['WORKSHOP'] == 'Wash and More':
         data['Link'] = 'https://cars.mekit.in/bookatwashandmore'
-    elif data['Workshop'] == 'Aher Auto':
+    elif data['WORKSHOP'] == 'Aher Auto':
         data['Link'] = 'https://cars.mekit.in/bookataherauto'
-    elif data['Workshop'] == 'Auto Trust':
+    elif data['WORKSHOP'] == 'Auto Trust':
         data['Link'] = 'https://cars.mekit.in/bookatautotrust'
-    elif data['Workshop'] == 'Landmark Renault':
+    elif data['WORKSHOP'] == 'Landmark Renault':
         data['Link'] = 'https://cars.mekit.in/bookatlandmarkrenault'
-    elif data['Workshop'] == 'Landmark Thane':
+    elif data['WORKSHOP'] == 'Landmark Thane':
         data['Link'] = 'https://cars.mekit.in/bookatlandmarkthane'
-    elif data['Workshop'] == 'Mannu Auto':
+    elif data['WORKSHOP'] == 'Mannu Auto':
         data['Link'] = 'https://cars.mekit.in/bookatmannuauto'
-    elif data['Workshop'] == 'Modi Hyundai':
+    elif data['WORKSHOP'] == 'Modi Hyundai':
         data['Link'] = 'https://cars.mekit.in/bookatmodihyundai'
-    elif data['Workshop'] == 'Regent Honda':
+    elif data['WORKSHOP'] == 'Regent Honda':
         data['Link'] = 'https://cars.mekit.in/bookatregenthonda'
-    elif data['Workshop'] == 'Satyam Ford':
+    elif data['WORKSHOP'] == 'Satyam Ford':
         data['Link'] = 'https://cars.mekit.in/bookatsatyamford'
-    elif data['Workshop'] == 'Sudarshan Tata':
+    elif data['WORKSHOP'] == 'Sudarshan Tata':
         data['Link'] = 'https://cars.mekit.in/bookatsudarshantata'
-    elif data['Workshop'] == 'Suzuki Thane':
+    elif data['WORKSHOP'] == 'Suzuki Thane':
         data['Link'] = 'https://cars.mekit.in/bookatsuzukithane'
-    elif data['Workshop'] == 'VW Thane':
+    elif data['WORKSHOP'] == 'VW Thane':
         data['Link'] = 'https://cars.mekit.in/bookatvwthane'
-    elif data['Workshop'] == 'Millennium Toyota':
+    elif data['WORKSHOP'] == 'Millennium Toyota':
         data['Link'] = 'https://cars.mekit.in/bookatmillenniumtoyota'
-    elif data['Workshop'] == 'Dealership':
+    elif data['WORKSHOP'] == 'Dealership':
         data['Link'] = 'https://cars.mekit.in/bookatdealership'
+    elif data['WORKSHOP'] == 'Workshop':
+        data['Link'] = 'https://cars.mekit.in/bookatgarageform'
 
-    # Calculate the monthly fuel cost based on "Fuel Avg run/month"
-    fuel_avg_run_per_month = data['Avg run/month']
+
+    # Calculate the monthly fuel cost based on "FUEL"
+    fuel_avg_run_per_month = data['FUEL']
     if pd.isna(fuel_avg_run_per_month):
             fuel_avg_run_per_month = 500  # Set a default value of 500
 
-    monthly_fuel_cost = (fuel_avg_run_per_month / mileage) * fuel_cost_per_liter
-    
+    monthly_fuel_cost = (fuel_avg_run_per_month / new_mileage) * new_fuel_cost_per_liter
+
     # Set a default value of 500 if it's less than or equal to 0
     if fuel_avg_run_per_month <= 0:
         fuel_avg_run_per_month = 500
-    
+
     # Get the current date in the format "DD/MM/YYYY"
     current_date = datetime.now().strftime("%d/%m/%Y")
     data['Last Update'] = current_date
 
     # Add the calculated monthly fuel cost to the data dictionary
-    data['Monthly Fuel Cost'] = monthly_fuel_cost
+    data['MAK'] = monthly_fuel_cost
 
     # Get the "Fuel Type" from the data
-    fuel_type = data.get('Fuel Type', 'Petrol')  # Default to Petrol if Fuel Type is missing
-    puc_renewal_cost = fuel_cost_per_type.get(fuel_type, 125)  # Default to 125 for unknown fuel types
+    fuel_type = data.get('FUEL', 'Petrol')  # Default to Petrol if Fuel Type is missing
+    puc_renewal_cost = new_fuel_cost_per_type.get(fuel_type, 125)  # Default to 125 for unknown fuel types
+    
+    
 
     # Add the PUC renewal cost and "Fuel Type" to the data dictionary
     data['PUC Renewal Cost'] = puc_renewal_cost
     data['Fuel Type'] = fuel_type  # Add or overwrite the value
-    
-    # Replace missing "Full name" with "Sir"
-    if pd.isna(data['Full name']) or data['Full name'] == '':
-        data['Full name'] = 'Sir'
+
+    # Replace missing "FULL NAME" with "Sir"
+    if pd.isna(data['FULL NAME']) or data['FULL NAME'] == '':
+        data['FULL NAME'] = 'Sir'
 
     # Calculate the fuel cost for 4 months
     fuel_cost_for_4_months = monthly_fuel_cost * 4
     data['Fuel Cost for 4 Months'] = fuel_cost_for_4_months
-    
 
     # Generate manifest JSON file based on the custom template
     manifest_data = json_template_data.copy()
@@ -123,7 +121,7 @@ for index, row in df.iterrows():
 
     # Generate JSON file with the data
     json_output = json.dumps(manifest_data, indent=4)
-    json_file_name = f'output_json/{index}.json'
+    json_file_name = f'{new_output_dir}/{index}.json'
     with open(json_file_name, 'w') as json_file:
         json_file.write(json_output)
 
@@ -131,13 +129,16 @@ for index, row in df.iterrows():
     data['Manifest_File_Number'] = index
 
     # Render the HTML using the template and data
-    html_output = template.render(data=data)
+    html_output = new_template.render(data=data)
 
     # Save the generated HTML to a file (one file per row)
-    output_file_name = f'output_html/{index}.html'
+    output_file_name = f'{new_output_dir}/{index}.html'
     with open(output_file_name, 'w') as output_file:
         output_file.write(html_output)
 
     print(f"HTML file {output_file_name} and manifest JSON file {json_file_name} generated successfully!")
 
 print("All HTML and JSON files generated successfully!")
+
+
+
